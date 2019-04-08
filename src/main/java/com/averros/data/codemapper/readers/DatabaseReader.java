32f6a514a00641c2e974cb75d0878c93c2be6556
Syed Wajahat Aziz <swajahataziz@hotmail.com>
2019-04-08 23:17:17 +0100

    Created the database reader class. Implementation to follow

---
+++ src/main/java/com/averros/data/codemapper/readers/DatabaseReader.java
@@ -0,0 +1,9 @@
+package com.averros.data.codemapper.readers;
+
+import reactor.core.publisher.Flux;
+
+public class DatabaseReader {
+
+    private Flux<String> data;
+    private String database;
+}
