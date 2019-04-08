ca0db5858b7de920795ab4818eb835a008cdae33
Syed Wajahat Aziz <swajahataziz@hotmail.com>
2019-04-08 15:51:33 +0100

    Updates to CsvReader: added read(Path, Delimiter)

---
+++ src/test/java/com/averros/data/codemapper/reader/CsvReaderTest.java
@@ -0,0 +1,26 @@
+package com.averros.data.codemapper.reader;
+
+import com.averros.data.codemapper.readers.CsvReader;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.List;
+
+@SpringBootTest
+public class CsvReaderTest {
+
+    @Test
+    public void testReadFile() {
+
+        Path path = Paths.get("/Users/wajahat/test.csv");
+
+        Flux<List<String>> flux = CsvReader.readFile(path,',');
+        Assert.assertNotNull(flux);
+
+    }
+}
